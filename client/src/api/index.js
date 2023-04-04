ca2bd0e22c203643f3799af72c13e687525063f1
shanu-user <shanunandanwar12@gmail.com>
2023-04-04 12:36:46 -0500

    Changed url of backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API=axios.create({baseURL: 'http://localhost:5000'})
+const API=axios.create({baseURL: 'http://starter-micro-api-eight.vercel.app'})
 
 API.interceptors.request.use((req) =>{
     // console.log(process.env.REACT_APP_JWT_SECRET_TOKEN)
